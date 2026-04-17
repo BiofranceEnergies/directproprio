85c48d6c1d9eb90bc77ebba31e7a9a5de148f50f
BiofranceEnergies <sylvain.matignon@gmail.com>
2026-04-17 14:21:25 +0200

    Update heroVideoUrl in data.js

--- mandat-446712/data.js
+++ mandat-446712/data.js
@@ -11,7 +11,7 @@ const HouseData = {
     rooms: "5",
     bedrooms: "3",
     land: "5 950 m²",
-    heroVideoUrl: "drone-446712-github.mp4",
+    heroVideoUrl: "sabouret-12.mp4",
     youtubeID: "fanHHIuEACI", 
     description: [
         "Située dans un environnement calme et préservé, cette magnifique propriété de caractère offre une alliance parfaite entre le charme de l'ancien et l'élégance contemporaine.",
